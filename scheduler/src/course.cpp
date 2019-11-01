8314e00daa265cc94817eded4a308b83dc2c49e1
lePerdu <zdpeltzer@gmail.com>
2019-11-01 19:23:39 -0400

    Revert newer Course::print_course code
    Also removed a superfluous `const` in Course::get_credits(); returning a
    `const` value (i.e. not by pointer or reference) doesn't mean anything
    as it will be copied to the caller anyway.

--- scheduler/src/course.cpp
+++ scheduler/src/course.cpp
@@ -1,6 +1,8 @@
 
 #include "course.hpp"
+
 #include <algorithm>
+#include <iostream>
 
 Course::Course(std::string name, std::uint8_t credits) :
     name(name), credits(credits) {}
@@ -9,7 +11,7 @@ const std::string& Course::get_name() const {
     return name;
 }
 
-const uint8_t Course::get_credits() const {
+std::uint8_t Course::get_credits() const {
     return credits;
 }
 
@@ -21,6 +23,8 @@ bool Course::has_prereq(const Course* prereq) const {
     return std::find(prereqs.begin(), prereqs.end(), prereq) != prereqs.end();
 }
 
-void Course::print_course(){
-    printf("Course Title: [%s]\tCredits: [%u]\n", Course::name.c_str(), Course::credits);
+void Course::print_course() const {
+    std::cout << "Course Title: [" << name.c_str() << "]"
+              << "\t"
+              << "Credits: [" << credits << "]" << std::endl;
 }
--- scheduler/src/course.hpp
+++ scheduler/src/course.hpp
@@ -8,10 +8,10 @@ class Course {
 public:
     Course(std::string name, std::uint8_t credits);
     const std::string& get_name() const;
-    const std::uint8_t get_credits() const;
+    std::uint8_t get_credits() const;
     void add_prereq(const Course* prereq);
     bool has_prereq(const Course* prereq) const;
-    void print_course();
+    void print_course() const;
 
 private:
     std::string name;
