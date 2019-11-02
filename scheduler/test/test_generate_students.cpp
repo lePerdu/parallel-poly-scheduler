3d4cbd430b3a677b00c5fac90b454ea1083b408a
lePerdu <zdpeltzer@gmail.com>
2019-11-01 20:06:34 -0400

    Update test_generate_students.cpp

--- scheduler/test/test_generate_students.cpp
+++ scheduler/test/test_generate_students.cpp
@@ -1,24 +1,26 @@
-#include <iostream>
-#include "generate_students.cpp"
 #include "course.cpp"
 #include "parse_json.cpp"
-//#include "generate_students.hpp"
+#include "student.cpp"
 
-int main(){
+#include <iostream>
 
+int main() {
 
     // Get a list of all courses available to cs majors
     ParseJson parsed_list("cs_major_courses.json");
-    std::vector<Course> available_courses = parsed_list.get_course_list();
+    auto available_courses = parsed_list.get_course_list();
 
-    // Create a List of students based on the number of students and the list of available courses
-    GenerateStudents Students(500, available_courses);
-    Students.print_all_students_courses();
-
-    std::vector<Student> students = Students.get_student_list();
-
-    for (int i = 0; i < students.size(); i++){
-        printf("Student id: [%u] -- Taken Credits: [%u]\n", students.at(i).student_id, students.at(i).taken_credits);
+    // This is kinda annoying. Maybe have generate_random_students take the
+    // vector a assume that poitners into it will not change?
+    std::vector<const Course*> course_pointers(available_courses.size());
+    for (auto& course : available_courses) {
+        course_pointers.push_back(&course);
     }
+
+    // Create a List of students based on the number of students and the list of
+    // available courses
+    auto students = generate_random_students(500, course_pointers);
+    print_all_students_courses(students);
+
     return 0;
 }
